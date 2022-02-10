23937e38403aa22f43564a1282c4dd045b433731
Jonatan <jonatan.ramr@gmail.com>
2022-02-10 11:44:06 +0100

    Ejercicio 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+
+  const [data, setData] = useState({
+    text: [
+      {
+        title:'Una página web',
+        checked: false,
+        price: 500
+      },
+      {
+        title: 'Una consultoría SEO',
+        checked: false,
+        price: 300
+      },
+      {
+        title: 'Una campaña de Google Adds',
+        checked: false,
+        price: 200
+      }
+    ],
+    pagesNumber: "",
+    languagesNumber: "",
+    total: 0
+  });
+
+  const budgetList = data.text.map((item, index) => {
+    return <label><input type="checkbox" value={index} onChange= {(e) => isChecked(e, index)}/>{` ${item.title} (${item.price}€)`}</label>
+  })
+
+  const isChecked = (e, index) => {
+    data.text.map((item, index2) => {
+      if(index === index2 && e.target.checked === true) {
+        item.checked = true;
+        data.total = data.total + item.price;
+        setData({...data});
+      } else if((index === index2 && e.target.checked === false)) {
+        item.checked = false;
+        data.total = data.total - item.price;
+        console.log("adios");
+        setData({...data});
+      }
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h3 className='ms-3 mt-3'>¿Qué quieres hacer?</h3>
+      <form className='formulary'>
+        {budgetList}
+        <p>{`Precio: ${data.total}€`}</p> 
+      </form>
+    </div> 
   );
 }
 
